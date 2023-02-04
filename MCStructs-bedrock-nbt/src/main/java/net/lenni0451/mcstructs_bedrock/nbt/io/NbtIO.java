06f4ebee843f901ad131938cda0791b828091fc1
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2023-02-04 20:15:48 +0100

    Added javadoc

--- MCStructs-bedrock-nbt/src/main/java/net/lenni0451/mcstructs_bedrock/nbt/io/NbtIO.java
+++ MCStructs-bedrock-nbt/src/main/java/net/lenni0451/mcstructs_bedrock/nbt/io/NbtIO.java
@@ -5,6 +5,9 @@ import net.lenni0451.mcstructs_bedrock.nbt.io.impl.BedrockNbtWriter;
 
 public class NbtIO {
 
+    /**
+     * Implementation of the Bedrock Edition Nbt format.
+     */
     public static final net.lenni0451.mcstructs.nbt.io.NbtIO BEDROCK = new net.lenni0451.mcstructs.nbt.io.NbtIO(new BedrockNbtReader(), new BedrockNbtWriter());
 
 }
