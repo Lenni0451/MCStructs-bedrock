6eeab0219d48aa33c2ba12c4f27b086da3a76066
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2023-02-05 13:14:01 +0100

    Added javadoc

--- MCStructs-bedrock-text/src/main/java/net/lenni0451/mcstructs_bedrock/text/utils/BedrockTranslator.java
+++ MCStructs-bedrock-text/src/main/java/net/lenni0451/mcstructs_bedrock/text/utils/BedrockTranslator.java
@@ -15,7 +15,17 @@ public class BedrockTranslator {
 
     /**
      * Translate a key with the given translator and arguments.<br>
-     * This method is made to comply with the vanilla bedrock client.
+     * This method is made to comply with the vanilla bedrock client.<br>
+     * <br>
+     * The bedrock edition has a special way of appending data to a translation key:<br>
+     * If only the key is given, the client will try to translate it.<br>
+     * Translation keys <i>can</i> start with a {@code %} sign. When having multiple translation keys in a single message this is required.<br>
+     * Translation keys can contain the following characters: {@code a-z, A-Z, 0-9, _, -, .}<br>
+     * If the key ends with any other character, the client will append it to the translated string (even unescaped {@code %} signs).<br>
+     * To insert a {@code %} sign into the message, you have to escape it with another {@code %} sign.<br>
+     * All {@code $d} and {@code $s} will be removed from the translated string.<br>
+     * {@code %d}, {@code %s} and {@code %[1-9]} will be replaced with the given arguments.<br>
+     * When specifying a number, the argument of the given index will be used. The index starts at 1 and is offset by the number of {@code %s} and {@code %d} in the key.
      *
      * @param key        The key to translate
      * @param translator The translator function
@@ -36,6 +46,7 @@ public class BedrockTranslator {
             int matchEnd = matcher.end();
             if (matchStart > start) out.append(translated, start, matchStart);
             start = matchEnd;
+
             String match = matcher.group(1);
             if (match.equals("d") || match.equals("s")) appendArg(out, getArg(args, argIndex++));
             else appendArg(out, getArg(args, numArgOffset + Integer.parseInt(match) - 1));
